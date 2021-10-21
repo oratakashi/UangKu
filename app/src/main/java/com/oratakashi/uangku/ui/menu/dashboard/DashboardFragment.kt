ec01f4227aa116f1a8914334cee6e648d30ef084
rohmanureldi <eldirohmanur@gmail.com>
2021-10-21 19:41:07 +0700

    Refactoring Summary component to Transaction component

--- app/src/main/java/com/oratakashi/uangku/ui/menu/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/oratakashi/uangku/ui/menu/dashboard/DashboardFragment.kt
@@ -5,20 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.ExperimentalMaterialApi
+import android.widget.Toast
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import com.oratakashi.uangku.R
-import com.oratakashi.uangku.component.SummaryList
+import com.oratakashi.uangku.component.widget.TransactionList
 import com.oratakashi.uangku.databinding.FragmentDashboardBinding
-import com.oratakashi.uangku.models.SummaryItem
-import com.oratakashi.uangku.models.SummaryItemColor
+import com.oratakashi.uangku.models.TransactionCardColor
+import com.oratakashi.uangku.models.TransactionCardType
+import com.oratakashi.uangku.models.TransactionData
 import com.oratakashi.viewbinding.core.binding.fragment.viewBinding
 import com.oratakashi.viewbinding.core.tools.onClick
 import com.oratakashi.viewbinding.core.tools.toast
 
 class DashboardFragment : Fragment() {
 
-    @ExperimentalMaterialApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
@@ -27,27 +26,39 @@ class DashboardFragment : Fragment() {
             summaryListView.apply {
                 setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
                 setContent {
-                    SummaryList(
-                        summaryItems = listOf(
-                            SummaryItem(
-                                10000000,
-                                "Makanan",
-                                SummaryItemColor.GREEN
+                    TransactionList(
+                        cardType = TransactionCardType.DETAIL,
+                        onItemClick = {
+                            Toast.makeText(requireContext(), it.category, Toast.LENGTH_SHORT).show()
+                        },
+                        transactionItems = listOf(
+                            TransactionData(
+                                total = 10000000,
+                                category = "Makanan",
+                                color = TransactionCardColor.GREEN,
+                                date = "17 Oktober 2021",
+                                description = "KFC"
                             ),
-                            SummaryItem(
-                                500000,
-                                "Reksa dana",
-                                SummaryItemColor.RED
+                            TransactionData(
+                                total = 500000,
+                                category = "Reksa dana",
+                                color = TransactionCardColor.RED,
+                                date = "18 Oktober 2021",
+                                description = "Sucor"
                             ),
-                            SummaryItem(
-                                250000,
-                                "Kebutuhan Harian",
-                                SummaryItemColor.CYAN
+                            TransactionData(
+                                total = 250000,
+                                category = "Kebutuhan Harian",
+                                color = TransactionCardColor.CYAN,
+                                date = "19 Oktober 2021",
+                                description = "Skincare"
                             ),
-                            SummaryItem(
-                                1000000,
-                                "Diamond Mobile Legend",
-                                SummaryItemColor.BLUE
+                            TransactionData(
+                                total = 1000000,
+                                category = "Diamond Mobile Legend",
+                                color = TransactionCardColor.BLUE,
+                                date = "20 Oktober 2021",
+                                description = "Skin legend badang"
                             ),
                         )
                     )
