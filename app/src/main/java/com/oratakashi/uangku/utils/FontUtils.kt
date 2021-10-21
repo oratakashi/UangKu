575fc40dd727a9c08690d10d0b7f8a522ecf55d5
rohmanureldi <eldirohmanur@gmail.com>
2021-10-21 16:43:15 +0700

    Added CurrencyUtils & FontUtils

---
+++ app/src/main/java/com/oratakashi/uangku/utils/FontUtils.kt
@@ -0,0 +1,16 @@
+package com.oratakashi.uangku.utils
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.oratakashi.uangku.R
+
+val montserratFont = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_regular, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_heavy, FontWeight.ExtraBold)
+)
